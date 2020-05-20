dd6bc7dc8614f128edec61cd14bd1849644b36c5
Restyled.io <commits@restyled.io>
2020-05-20 14:05:34 +0000

    Restyled by shellharden

--- src/platform/i386/qemu-mp.sh
+++ src/platform/i386/qemu-mp.sh
@@ -4,7 +4,7 @@ if [ $# != 1 ]; then
   exit 1
 fi
 
-if ! [ -r $1 ]; then
+if ! [ -r "$1" ]; then
   echo "Can't open run-script"
   exit 1
 fi
@@ -15,6 +15,6 @@ else
   KVM_FLAG=""
 fi
 
-MODULES=$(sh $1 | awk '/^Writing image/ { print $3; }' | tr '\n' ' ')
+MODULES=$(sh "$1" | awk '/^Writing image/ { print $3; }' | tr '\n' ' ')
 
-qemu-system-i386 $KVM_FLAG -m 768 -smp 4 -nographic -kernel kernel.img -no-reboot -s -initrd "$(echo $MODULES | tr ' ' ',')"
+qemu-system-i386 "$KVM_FLAG" -m 768 -smp 4 -nographic -kernel kernel.img -no-reboot -s -initrd "$(echo "$MODULES" | tr ' ' ',')"
