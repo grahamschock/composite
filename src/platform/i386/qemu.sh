9ea0c27fe7a2e95ab84e9a0f429197c0b17716e9
Restyled.io <commits@restyled.io>
2020-05-20 14:05:35 +0000

    Restyled by shfmt

--- src/platform/i386/qemu.sh
+++ src/platform/i386/qemu.sh
@@ -11,11 +11,10 @@ fi
 
 if [ -e "/dev/kvm" ] && [ -r "/dev/kvm" ] && [ -w "/dev/kvm" ]; then
   KVM_FLAG="-enable-kvm"
-else 
+else
   KVM_FLAG=""
 fi
 
 MODULES=$(sh "$1" | awk '/^Writing image/ { print $3; }' | tr '\n' ' ')
 
 qemu-system-i386 "$KVM_FLAG" -m 768 -nographic -kernel kernel.img -no-reboot -s -initrd "$(echo "$MODULES" | tr ' ' ',')"
-
